e45c5aa97c72b48ead0587a54116521855658191
just a random! <79994051+justarandom-1@users.noreply.github.com>
2026-08-12 23:32:58 -0600

    Build 08-12-26

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Team Swagger-Until We Get Love Right-1.0";
+const cacheName = "Team Swag-Until We Get Love Right-1.0";
 const contentToCache = [
     "Build/Build.loader.js",
     "Build/Build.framework.js",
